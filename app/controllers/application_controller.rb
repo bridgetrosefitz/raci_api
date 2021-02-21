efd71c639ee8c41a543a99251b2fb23c308a1b54
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-02-21 22:21:45 +0100

    Refactors JWT auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+  def token
+    request.headers["Authorizaion"].split(" ")[1]
+  end
+
+  def decoded_token
+    JWT.decode(token, "codingiscool", true, { algorithm: "HS256"})
+  end
+
+  def current_user
+    user_id = decoded_token
+  end
+
 end
