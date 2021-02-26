7bc1d9c1814633942f3bb4e42aab049e93fd07d0
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-02-26 23:54:41 +0100

    Work in progress - customizing task serializer to show the stuff I need for my Project page

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,4 +1,17 @@
 class TaskSerializer
   include FastJsonapi::ObjectSerializer
   attributes :text, :project_id, :user_tasks, :members
+  
+  # Example code from https://medium.com/@natj/creating-custom-attributes-for-serialization-bd5518514544
+  # attributes :high_score do |object|
+  #   object.games.map(&:score).max
+
+    attributes :user_task_user_full_name do |task|
+      task.user_tasks.map {|user_task| user_task.user.full_name}
+    end
 end
+        # Things I need in my endpoint
+
+        # - UserTask
+        # - (Attribute): user_task.user.full_name
+        # - (Attribute): user_task.function.name
