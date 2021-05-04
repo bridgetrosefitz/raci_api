09df61ac4d5daa697d3565c6db79158a7d9c9503
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-05-04 15:26:44 -0400

    Added basic instances to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# Function.create!(name:"Responsible", description:"The person who carries out the work to complete this task") 
-# Function.create!(name:"Accountable", description: "The person who must ensure that the work gets done, and who will bear the consequences if it does not get done.")
-# Function.create!(name:"Consulted", description: "The person or people whose input must be sought for key decisions while carrying out the work.")
-# Function.create!(name:"Informed", description: "The person or people who must be kept informed about progress and key developments.")
+Function.create!(name:"Responsible", description:"The person who carries out the work to complete this task") 
+Function.create!(name:"Accountable", description: "The person who must ensure that the work gets done, and who will bear the consequences if it does not get done.")
+Function.create!(name:"Consulted", description: "The person or people whose input must be sought for key decisions while carrying out the work.")
+Function.create!(name:"Informed", description: "The person or people who must be kept informed about progress and key developments.")
+
+User.create!(first_name: "Bridget", last_name: "Fitzgerald", email: "bridgetrosefitz@gmail.com", password: "test")
+User.create!(first_name: "Kevin", last_name: "McAlear", email: "kevin.mcalear@gmail.com", password: "test")
+User.create!(first_name: "Patrick", last_name: "Fitzgerald", email: "patrick@test.com", password: "test")
+
+Project.create!(name: "Creating Bridget's first project", creator_id: 1)
+Project.create!(name: "Creating Kevin's first project", creator_id: 2)
+Project.create!(name: "Creating Patrick's first project", creator_id: 3)
+
+Task.create!(text: "Define your first task", project_id: 1)
+
+UserTask.create!(task_id: 1, function_id: 1, user_id: 1)
+UserTask.create!(task_id: 1, function_id: 2, user_id: 1)
+UserTask.create!(task_id: 1, function_id: 3, user_id: 2)
+UserTask.create!(task_id: 1, function_id: 4, user_id: 2)
