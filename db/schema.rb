05e554e341168613d200cbf02d9d0401de2a375e
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-05-18 18:48:41 -0400

    Moves database from sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_14_192313) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "flags", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "task_id", null: false
