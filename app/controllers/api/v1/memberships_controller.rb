ea52928077ae23c063fbb0483b4ee38cf83cd0c0
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-05-20 18:03:57 -0400

    Validates membership so only one user per project; updates Membership controller so that error + message is returned if not valid on create

--- app/controllers/api/v1/memberships_controller.rb
+++ app/controllers/api/v1/memberships_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::MembershipsController < ApplicationController
    def create
-    membership = Membership.create!(membership_params)
-    render json: membership
+    membership = Membership.new(membership_params)
+    if membership.valid?
+      membership.save!
+      render json: membership 
+    else
+      render json: { errors: membership.errors.full_messages }, status: :unprocessable_entity 
+    end
   end
 
   def destroy
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,7 @@
 class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :project
+  validates :user, uniqueness: {scope: :project, message: 'is already a member of this project.'}
+  
+  
 end
